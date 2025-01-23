c229db0414a3d1dc8289e768edb2868ffe17900a
kishore1528 <mkishorekumar1528@gmail.com>
2025-01-23 20:44:39 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        String username = "kishore";
+        String password = "kishore1528";
+        while(username == "kishore" && password == "kishore1528") {
+            System.out.println("Hello user Happy new year");
+            break;
+        }
+    }
+    
+}
