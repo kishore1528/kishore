f43f7b2e39f2fd171f5c073e66b0d900f776d92d
kishore1528 <mkishorekumar1528@gmail.com>
2025-02-04 19:56:23 +0530

    Add files via upload

---
+++ Demo003.java
@@ -0,0 +1,20 @@
+public class Demo003 {
+    public static void main(String[] args) {
+        
+
+        int x = 8;
+        int y = 9;
+        int z = 10;
+        if(x > y) {
+            System.out.println(x);
+        }
+        else if(y > z) {
+            System.out.println(y);
+
+        }
+        else {
+            System.out.println(z);
+        }
+
+    }
+}
